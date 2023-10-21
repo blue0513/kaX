04378940a4a808ee7cd471d67c729c24b2e1610d
Taiju Aoki <aokitaiju0513@gmail.com>
2023-10-22 01:05:52 +0900

    update icon

--- worker.js
+++ worker.js
@@ -1,8 +1,51 @@
-chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
-  if (!url.startsWith("https://twitter.com/")) return;
+let kaxEnabled = true;
 
-  chrome.scripting.insertCSS({
+chrome.webNavigation.onDOMContentLoaded.addListener(async ({ tabId, url }) => {
+  if (!url.startsWith("https://twitter.com/")) {
+    chrome.action.disable(tabId);
+    return;
+  } else {
+    chrome.action.enable(tabId);
+  }
+
+  const enabled = getEnabled();
+  const injectionCSS = {
     target: { tabId: tabId },
     files: ["style.css"],
-  });
+  };
+
+  toggleCSS(enabled, injectionCSS);
+  toggleIcon(enabled);
 });
+
+chrome.action.onClicked.addListener(() => {
+  setEnabled(!getEnabled());
+  reload();
+});
+
+function setEnabled(value) {
+  kaxEnabled = value;
+}
+
+function getEnabled() {
+  return kaxEnabled;
+}
+
+function reload() {
+  chrome.tabs.reload();
+}
+
+function toggleCSS(enabled, css) {
+  if (enabled) {
+    chrome.scripting.insertCSS(css);
+  } else {
+    chrome.scripting.removeCSS(css);
+  }
+}
+
+function toggleIcon(enabled) {
+  const icon = enabled
+    ? "favicon/favicon16.png"
+    : "favicon/favicon_disabled.png";
+  chrome.action.setIcon({ path: icon });
+}
